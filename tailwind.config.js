6398a1326835dd2ae74f6b989d23e5f4211bebc1
Bert <you@example.combertwojo5@gmail.com>
2025-07-22 19:56:08 -0400

    Fixed Tailwind content configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // THIS LINE IS CRITICAL: It tells Tailwind where to find your classes
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
